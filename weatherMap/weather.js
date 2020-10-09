7ae9a1d8d9dde5733e15b4b7ae943ed08c5ce7be
faithgarza001 <faith.garza001@gmail.com>
2020-10-09 18:16:44 -0500

    changes

--- weatherMap/weather.js
+++ weatherMap/weather.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
-    var proxy = 'https://cors-anywhere.herokuapp.com/';
-    var apiLinkDS = "https://api.darksky.net/forecast/"+ DARKSKY_SECRET +"/37.8267,-122.4233";
+    let proxy = 'https://cors-anywhere.herokuapp.com/';
+    let apiLinkDS = "https://api.darksky.net/forecast/"+ DARKSKY_SECRET +"/37.8267,-122.4233";
 
     $.ajax({
         url: proxy + apiLinkDS,
@@ -9,3 +9,13 @@ $(document).ready(function() {
     });
 
 });
+
+//create a request
+let request = new XMLHttpRequest();
+
+//open connection using get
+request.open('GET','https://api.darksky.net/forecast/DARKskyKEy/37.8267,-122.4233',true)
+request.onload = function (){
+
+    console.log(data)
+}
