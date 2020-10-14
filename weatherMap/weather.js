ac88cdb0313462e87522d87ba85bf2c9b4b5b8d1
faithgarza001 <faith.garza001@gmail.com>
2020-10-13 20:22:04 -0500

    finshed minimum viable weather map project

--- weatherMap/weather.js
+++ weatherMap/weather.js
@@ -1,92 +1,140 @@
-$(document).ready(function() {
+/**JavaScript has available built-in browser objects and some external open source libraries to interact with API's.
+//All the possible ways of making an API call in JavaScript
+ 1)XMLHttpRequest
 
-    let proxy = 'https://cors-anywhere.herokuapp.com/';
-    let apiLinkDS = "https://api.darksky.net/forecast/"+ DARKSKY_SECRET +"/37.8267,-122.4233";
+ 2) fetch
 
-    $.ajax({
-        url: proxy + apiLinkDS,
-        success:function(data) { console.log(data);}
-    });
+ 3) Axios
 
-});
+ 4) jQuery*/
 
 
-const container = document.getElementById('root');
 
-let request = new XMLHttpRequest()
-request.open('Get','https://api.darksky.net/forecast/'+ DARKSKY_SECRET +'/37.8267,-122.4233' )
+//XMLHttpRequest- Before ES6 the standard way to make HTTP request was using XMLHttpRequest. The XMLHttpRequest is built within the browser and allows to be made use in HTTP request in JavaScript since most browsers have javascript embedded. This method has been deprecated (outdated) once the introduction of fetch within ES 6 arrival.
 
 
-const wDay = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-const wMonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-const iconValue = {
-    CLEARDAY: 'clear-day',
-    CLEARNIGHT: 'clear-night',
-    RAIN: 'rain',
-    SNOW: 'snow',
-    SLEET: 'sleet',
-    WIND: 'wind',
-    FOG: 'fog',
-    CLOUDY: 'cloudy',
-    PARTLY_CLOUDY_DAY: 'partly-cloudy-day',
-    PARTLY_CLOUDY_NIGHT: 'partly-cloudy-night'
+// let request = new XMLHttpRequest();
+// request.open('GET',`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/`+DARKSKY_SECRET+`/37.8267,-122.4233`);
+// request.send();
+// request.onload = () => {
+//     if(request.status == 200){
+//         console.log(JSON.parse(request.response));
+//     } else {
+//         console.log(` error ${request.status} ${request.statusText}`)
+//     }
+// }
+
+
+//FETCH-allows to make HTTP request similarly to XMLHttpRequest but with straightforward interface by using promises. Supported by most modern browsers, Fetch can be applied in two ways.
+
+
+
+//Method 1
+// fetch("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/"+DARKSKY_SECRET+"/42.3601,-71.0589")
+//     .then((response) => {
+//         return response.json();
+//     })
+//     .then((myJson) => {
+//         console.log(myJson);
+//     }).catch((error) => {
+//     console.log(error)
+// });
+
+
+// //USING ASYNC AND AWAIT-The disadvantage is the error handling within the fetch is not presented.
+// async function getWeather(){
+//     let response = await fetch("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/"+DARKSKY_SECRET+"/42.3601,-71.0589");
+//     let data = await  response.json()
+//     return data;
+// }
+//
+// getWeather().then(data => console.log(data));
+
+
+//THE NEXT WAY TO MAKE AN API REQUEST IS WITH AXIOS
+//Axios- an HTTP request open-source library, contains many features, functionality within browsers and Node.js, promise based but can be used vanilla js and React, Angular and other advanced frameworks. Axios automatically returns the JSON, performs better error handling as well a plethora of supported browsers
+
+
+
+
+
+// axios.get("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/"+DARKSKY_SECRET+"/42.3601,-71.0589")
+//     .then(response => {
+//         console.log(response.data);
+//     })
+//     .catch(error =>console.error(error));
+
+
+
+//jQuery
+// $(document).ready(function() {
+//
+//     let proxy = 'https://cors-anywhere.herokuapp.com/';
+//     let apiLinkDS = "https://api.darksky.net/forecast/"+ DARKSKY_SECRET +"/37.8267,-122.4233";
+//
+//     $.ajax({
+//         url: proxy + apiLinkDS,
+//         success:function(data) { console.log(data);}
+//     });
+//
+// });
+//
+import (openWeatherMap)
+/**THIS IS THE END OF API EXAMPLE BELOW BEGINS THE WEATHER APP**/
+
+const api = {
+    key: openWeatherMap,
+    base: "https://api.openweathermap.org/data/2.5/"
 }
 
-//fetching the weather
-function fetchWeatherReport(DARKSKY_SECRET,latitude,longitude){
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
 
-    let proxy = 'https://cors-anywhere.herokuapp.com/';
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+        getResults(searchbox.value);
 
-    let apiLinkDS = "https://api.darksky.net/forecast/"+ DARKSKY_SECRET +"/37.8267,-122.4233";
-
-    fetch(apiLinkDS)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-
-            //work with json data, you can see these objects in JSON format in console in browser, creating our own variables and assigning the JSON data object to our variables.
-            let resultsHTML = "";
-            let tableHTML = "";
-            let summary = data.currently.summary;
-            let temperature = data.currently.temperature;
-            let icon = data.currently.icon;
-            let precipitationProbability = data.currently.precipitationProbability;
-            let humidity = data.currently.humidity;
-            let windSpeed = data.currently.windSpeed;
-            let timeS = new Date(data.currently.time * 1000);
-            let forecastDate = `${wDay[timeS.getDay()]} ${wMonth[timeS.getMonth()]} ${timeS.getDate()}`
-
-
-            //Setting values for the current conditions
-            //using document get element by ID to place the location on front end layout
-            document.getElementById('dayTime').innerHTML = forecastDate;
-            document.getElementById('summary').innerHTML = summary;
-            document.getElementById('currentTemp').innerHTML = `${Math.round(temperature)}&deg`;
-            document.getElementById('WeatherIcon').src = getIcon(icon);
-            document.getElementById('precipitation').innerHTML = `Precipitation ${precipitationProbability*100}%`;
-            document.getElementById('humidity').innerHTML = `Humidity ${Math.round(windSpeed)}%`;
-            document.getElementById('wind').innerHTML = `Winds ${Math.round(windSpeed)}mph`;
-
-            //generate tabs
-
-            document.getElementById('dailyForecast').innerHTML = renderWeeklyForecast(data.daily);
-            document.getElementById('weeklyForecast').innerHTML = renderDailyForecast(data.hourly);
-        })
-        .catch(err => {
-            //On catch of error do the below
-            throw (`Sorry an error has occurred please check your connection. ${err}`);
-        })
+    }
 }
 
+function getResults (query) {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(weather => {
+            return weather.json();
+        }).then(displayResults);
+}
 
-function fetchLocation(DARKSKY_SECRET, latitude, longitude){
-
-    //
+function displayResults (weather) {
 
 
+    let city = document.querySelector('.location .city');
+
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
 
 
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
 
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+}
+
+function dateBuilder (d) {
+     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
 }
