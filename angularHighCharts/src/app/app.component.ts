1a8d105897d359fed8345e66e5c142210b92a183
faithgarza001 <faith.garza001@gmail.com>
2020-10-28 21:55:55 -0500

    adding angular

--- angularHighCharts/src/app/app.component.ts
+++ angularHighCharts/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import {donutCharOptions} from './helpers/donutChartOptions';
+import {Chart} from 'highcharts';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularHighCharts';
+  chart = new Chart(donutCharOptions);
 }
